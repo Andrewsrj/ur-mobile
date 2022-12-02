f60328d822545c46569a295b30378316c67fd082
AndC <andrews.mp@live.com>
2022-12-02 10:02:12 -0300

    Added Bio and University

--- pages/user/Home/Profile/index.js
+++ pages/user/Home/Profile/index.js
@@ -1,68 +1,86 @@
 import { useEffect, useState } from "react";
 import avatar from "../../../../components/avatar";
 import userService from "../../../../services/UserManager";
-import { Avatar, BioText, ButtonText, CollegeName, Container, DescriptionRow, DescriptionText, DisplayName, ImageRow, Rect, Rect2, RectItems,  SubDescriptionText, TouchableButton } from "./styles";
+import { Avatar, BioText, ButtonText, CollegeName, Container, DescriptionRow, DescriptionText, DisplayName, ImageRow, Rect, Rect2, RectItems, SubDescriptionText, TouchableButton } from "./styles";
 
 
 export function Profile() {
-    const user = userService.getUser();
-    
-    useEffect(() => {
+  const user = userService.getUser();
+  const [state, setState] = useState({
+    bio: "carregando dados...",
+    university: "carregando dados...",
+  })
+  const { bio, university } = state
 
-    }, []);
+  const updateState = (data) => setState((state) => ({ ...state, ...data }));
 
-    /**
-     * Remove a autenticação de um usuário
-     */
-    const handleSignout = () => {
-        userService.signout()
-        .then()
-        .catch(error => {
-          console.log(error);
+  useEffect(() => {
+    userService.getDataUser()
+      .then((dataUser) => {
+        updateState({
+          bio: dataUser.bio,
+          university: dataUser.university,
         })
-      }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []);
 
-    return (
-      <Container>
-        <Rect>
-          <RectItems>
-            <ImageRow>
-              <Avatar
-                source={avatar.getAvatar(user.photoURL)}
-                resizeMode="contain"
-              />
-              <DisplayName>{user.displayName}</DisplayName>
-              <CollegeName>UERJ/ZO</CollegeName>
-            </ImageRow>
-            <DescriptionRow>
-              <DescriptionText>000</DescriptionText>
-              <SubDescriptionText>Kms</SubDescriptionText>
-            </DescriptionRow>
-            <DescriptionRow>
-              <DescriptionText>000</DescriptionText>
-              <SubDescriptionText>Atividades</SubDescriptionText>
-            </DescriptionRow>
-            <DescriptionRow>
-              <DescriptionText>000</DescriptionText>
-              <SubDescriptionText>Conquistas</SubDescriptionText>
-            </DescriptionRow>
-          </RectItems>
-          <BioText>"A esperança é a última que morre!"</BioText>
-        </Rect>
-        <Rect2>
-          <TouchableButton>
-            <ButtonText>Editar</ButtonText>
-          </TouchableButton>
+  /**
+   * Remove a autenticação de um usuário
+   */
+  const handleSignout = () => {
+    userService.signout()
+      .then()
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  return (
+    <Container>
+      <Rect>
+        <RectItems>
+          <ImageRow>
+            <Avatar
+              source={avatar.getAvatar(user.photoURL)}
+              resizeMode="contain"
+            />
+            <DisplayName>{user.displayName}</DisplayName>
+            <CollegeName>{university}</CollegeName>
+          </ImageRow>
+          <DescriptionRow>
+            <DescriptionText>000</DescriptionText>
+            <SubDescriptionText>Kms</SubDescriptionText>
+          </DescriptionRow>
+          <DescriptionRow>
+            <DescriptionText>000</DescriptionText>
+            <SubDescriptionText>Atividades</SubDescriptionText>
+          </DescriptionRow>
+          <DescriptionRow>
+            <DescriptionText>000</DescriptionText>
+            <SubDescriptionText>Conquistas</SubDescriptionText>
+          </DescriptionRow>
+        </RectItems>
+        <BioText>"{bio}"</BioText>
+      </Rect>
+      <Rect2>
+        <TouchableButton>
+          <ButtonText>Editar</ButtonText>
+        </TouchableButton>
+        {!user.verified &&
           <TouchableButton>
             <ButtonText>Verificar</ButtonText>
           </TouchableButton>
-          <TouchableButton>
-            <ButtonText>Conquistas</ButtonText>
-          </TouchableButton>
-          <TouchableButton onPress={handleSignout}>
-            <ButtonText>Sair</ButtonText>
-          </TouchableButton>
-        </Rect2>
-      </Container>
-    );
-  }
+        }
+        <TouchableButton>
+          <ButtonText>Conquistas</ButtonText>
+        </TouchableButton>
+        <TouchableButton onPress={handleSignout}>
+          <ButtonText>Sair</ButtonText>
+        </TouchableButton>
+      </Rect2>
+    </Container>
+  );
+}
